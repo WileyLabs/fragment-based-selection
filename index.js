45afbc75244eaee366e2ef829a5d2cbc75f0c193
Benjamin Young <byoung2@wiley.com>
2018-05-23 12:07:56 -0400

    Improve error handling & reload experience

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-window.addEventListener("hashchange", () => {
+function selectFromFragment() {
   let hash = window.location.hash.slice(1);
+  if (hash == "") return;
+
   let split = hash.split('(');
   let type = split[0];
   let selector = decodeURIComponent(split[1].split(')')[0]);
@@ -21,5 +23,7 @@ window.addEventListener("hashchange", () => {
       console.log(`Error: selector of ${type} is invalid`);
   }
   console.log('found:', found);
+}
 
-}, false);
+window.addEventListener('hashchange', () => selectFromFragment(), false);
+document.addEventListener('DOMContentLoaded', () => selectFromFragment());
